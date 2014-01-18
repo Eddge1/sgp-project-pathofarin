2a670c0c02b4b585b2b4c5632b0c3beaee4cd9ba
Daniel <HakuroPve@gmail.com>
2014-01-18 15:05:33 -0500

    worked on CGame.h

--- source/Game.h
+++ source/Game.h
@@ -1,8 +1,73 @@
 #pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include "../SGD Wrappers/CSGD_Direct3D.h"
+#include "../SGD Wrappers/CSGD_DirectInput.h"
+#include "../SGD Wrappers/CSGD_TextureManager.h"
+#include "../SGD Wrappers/CSGD_XAudio2.h"
+
+#include <ctime>
+
+
 class CGame
 {
 public:
-	CGame(void);
-	virtual ~CGame(void);
+
+	// Singleton accessors:
+	static CGame* GetInstance( void );
+	static void DeleteInstance( void );
+
+
+	// Setup, Run, Cleanup
+	void Initialize( HWND hWnd, HINSTANCE hInstance,
+		int nWidth, int nHeight,
+		bool bIsWindowed );
+
+	void Run( void );
+
+	void Terminate( void );
+
+	// Window Accessors:
+	int GetScreenWidth ( void ) const	{	return m_nScreenWidth;	}
+	int GetScreenHeight( void ) const	{	return m_nScreenHeight;	}
+
+private:
+
+	// Static pointer to the singleton object
+	static CGame* s_pInstance;
+
+	// Private constructor / destructor to prevent multiple objects
+	CGame( void );						// default ctor
+	virtual ~CGame( void );				// dtor
+	CGame( const CGame& );				// copy ctor
+	CGame& operator= ( const CGame& );	// assignment op
+
+	// Helper Methods
+	bool Update( void );
+	void Render( void );
+	bool Load( void );
+	void GetFM( void );
+
+
+	// SGD Wrappers
+	CSGD_Direct3D*			m_pD3D;
+	CSGD_DirectInput*		m_pDI;
+	CSGD_TextureManager*	m_pTM;
+	CSGD_XAudio2*			m_pXA;
+
+	// Window Attributes
+	int						m_nScreenWidth;
+	int						m_nScreenHeight;
+	bool					m_bIsWindowed;
+
+	// Current Game State TODO:
+
+	// Current time
+	DWORD					m_dwCurrTime;
+
+
+
 };
 
