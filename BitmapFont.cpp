fc66a2c65e871ca7e0105e3e279a5b22c6b77e64
David B. Lindley <dblindley@fullsail.edu>
2014-01-18 20:00:29 -0500

    BitmapFont Mostly implemented, not quite functional

---
+++ BitmapFont.cpp
@@ -0,0 +1,85 @@
+#include "BitmapFont.h"
+
+#include "SGD Wrappers\CSGD_TextureManager.h"
+#include "TinyXML\tinyxml.h"
+
+CharDescriptor::CharDescriptor()
+{
+	m_nX = 0;
+	m_nY = 0;
+	m_nWidth = 0;
+	m_nHeight = 0;
+	m_nXOffset = 0;
+	m_nYOffset = 0;
+	m_nXAdvance = 0;
+	m_nPage = 0;
+}
+
+
+CBitmapFont::CBitmapFont(void)
+{
+
+}
+
+
+CBitmapFont::~CBitmapFont(void)
+{
+}
+
+
+void CBitmapFont::Initialize(int nID)
+{
+	m_nImageID = nID;
+}
+
+void CBitmapFont::Shutdown()
+{
+
+}
+
+bool CBitmapFont::FontParser()
+{
+	TiXmlDocument doc;
+	if (doc.LoadFile("assets/Fonts/test.fnt") == false)
+		return false;
+
+	TiXmlElement *pRoot = doc.RootElement();
+	if (pRoot == nullptr)
+		return false;
+
+	CharSet set;
+
+	TiXmlElement *pFont = pRoot->FirstChildElement("common");
+	if (pFont != nullptr)
+	{
+		pFont->Attribute("lineHeight", &set.m_nLineHeight);
+		pFont->Attribute("base", &set.m_nBase);
+		pFont->Attribute("scaleW", &set.m_nWidth);
+		pFont->Attribute("scaleH", &set.m_nHeight);
+		pFont->Attribute("pages", &set.m_nPages);
+	}
+
+	int count;
+	pFont = pRoot->FirstChildElement("chars");
+	if (pFont != nullptr)
+	{
+		pFont->Attribute("count", &count);
+	}
+
+	int charID;
+	pFont = pRoot->FirstChildElement("char");
+	for (int i = 0; i < count; i++)
+	{
+		pFont->Attribute("id", &charID);
+		pFont->Attribute("x", &set.Chars[charID].m_nX);
+		pFont->Attribute("y", &set.Chars[charID].m_nY);
+		pFont->Attribute("width", &set.Chars[charID].m_nWidth);
+		pFont->Attribute("height", &set.Chars[charID].m_nHeight);
+		pFont->Attribute("xoffset", &set.Chars[charID].m_nXOffset);
+		pFont->Attribute("yoffset", &set.Chars[charID].m_nYOffset);
+		pFont->Attribute("xadvance", &set.Chars[charID].m_nXAdvance);
+		pFont->Attribute("page", &set.Chars[charID].m_nPage);
+	}
+
+	return true;
+}
---
+++ BitmapFont.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+class CBitmapFont
+{
+public:
+	CBitmapFont(void);
+	~CBitmapFont(void);
+
+	//Initialize and Shutdown
+	void Initialize(int nID);
+	void Shutdown();
+
+	//Parse XML to load font
+	bool FontParser();
+	//Draw Text
+	void Draw(const TCHAR* szOutput, int nX, int nY, float fScale, DWORD dwColor) const;
+
+private:
+
+	int m_nImageID;
+
+
+
+};
+
+struct CharDescriptor
+{
+	int m_nX, m_nY;
+	int m_nWidth, m_nHeight;
+	int m_nXOffset, m_nYOffset;
+	int m_nXAdvance;
+	int m_nPage;
+
+	CharDescriptor();
+};
+
+struct CharSet
+{
+	int m_nLineHeight;
+	int m_nBase;
+	int m_nWidth, m_nHeight;
+	int m_nPages;
+	CharDescriptor Chars[256];
+};
