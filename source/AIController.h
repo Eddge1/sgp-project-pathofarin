224fc3bbf7bc7c545b1a01cf7360b4825e47195d
Daniel <HakuroPve@gmail.com>
2014-01-28 18:30:48 -0500

    Basic AI, and dodge event working

--- source/AIController.h
+++ source/AIController.h
@@ -1,7 +1,23 @@
 #pragma once
+#include "Minigames.h"
+#include "Units.h"
+
 class CAIController
 {
+
+	CMiniGames* m_pMiniGame;
+	CUnits* m_pOwner;
+
 public:
+
+	virtual void MakeOwner(CUnits* l) { m_pOwner = l; }
+	void AddMinigame(CMiniGames* l) { m_pMiniGame = l; }
+
+
+	virtual void Update(float fElapsedTime );
+	virtual void Render(void);
+
+
 	CAIController(void);
 	virtual ~CAIController(void);
 };
