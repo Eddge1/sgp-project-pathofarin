e46cd7089c84e44b705ff0a3839563b188b7eabf
James Bean <sirzodia@gmail.com>
2014-02-25 01:26:38 -0500

    Continue Bug Fix

--- source/ProfileMenuState.cpp
+++ source/ProfileMenuState.cpp
@@ -737,8 +737,8 @@ CPlayerUnit* CProfileMenuState::CreateTempPlayer(void)
 void CProfileMenuState::ContinueGame( int ID )
 {
 	string szZone = m_vCharacterList[ID-1]->GetZone() + ".xml";
-	CWorld* pWorld = CGamePlayState::GetInstance()->GetWorld(szZone);
 	CGamePlayState::GetInstance()->SetPlayer(m_vCharacterList[ID-1]);
+	CWorld* pWorld = CGamePlayState::GetInstance()->GetWorld(szZone);
 	for(unsigned int i = 0; i < m_vWorldData[ID-1].size();i++)
 		pWorld->AddClear(m_vWorldData[ID-1][i]);
 
